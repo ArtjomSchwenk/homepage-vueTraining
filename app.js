8edc396a0fad37062b750a7b3f552ffa5b7e1b38
Artjom <schwenk.artjom@tutamail.com>
2024-02-28 12:16:02 +0100

    experimenting with variables

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const app = Vue.createApp({
   template: `
-  <header>
-    <h1>Welcome to {{ name }} homepage!</h1>
+  <header> 
+    <h1>Welcome to {{ name }}'s homepage!</h1>
     <p>🚧This is my test site for learning vuejs und php🚧</p>
+    <img :src="banner">
   </header>
   `,
   data() {
     return {
-      name: 'Artjom'
+      name: 'Artjom',
+      banner: 'img/postcards.jpeg'
     }
   }
 });
