67f2f2dc6c9453c335117ba1b81b43e306b78733
Artjom <schwenk.artjom@tutamail.com>
2024-03-01 22:22:19 +0100

    added banner component to homepage

---
+++ homepage/src/components/Banner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="banner"> 
+    <img :src="image">
+    <div class="bannerText">
+      <h1>Welcome to {{ myName }}'s homepage!</h1>
+      <p>🚧This is my test site for learning vuejs und php🚧</p>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  data(){
+    return{
+      myName: 'Artjom',
+      image: require('@/assets/sunflowers.jpeg')
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0px;  
+}
+.banner {
+  position: relative;
+  text-align: center;
+}
+img {
+  height: 15rem;
+  width: 100%;
+  object-fit: cover;
+  filter: blur(2px);
+  filter: contrast(40%);
+}
+.bannerText {
+  color: white;
+  text-shadow: 0px 0px 8px #000000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+}
+</style>
+
---
+++ homepage/src/views/ProjectView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="projects">
+    <h1>This is where my projects will be displayed</h1>
+  </div>
+</template>
